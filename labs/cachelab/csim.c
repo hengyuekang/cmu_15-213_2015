1ac837e2b28420c2bfe9ad48b0e1bcdae5e25b7a
khy <khydom@qq.com>
2022-02-07 16:47:02 +0800

    cachelab partA done

--- labs/cachelab/csim.c
+++ labs/cachelab/csim.c
@@ -3,22 +3,77 @@
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
 /*
 name:hengyue kang
 */
-static int hit_count, miss_count, eviction_count, S, E, B;
-static char filename[1024];
-struct cache_line
+static int hit_count, miss_count, eviction_count, S, E, B,time_counter=0,b,s,verbose;
+static char filename[1024],buffer[1024];//skip "I"
+typedef struct
 {
     int valid, lru_counter;
-    int64_t tag; // stdint
-};
-static struct cache_line **cache;
+    unsigned long long tag; // stdint
+} cache_line;
+static cache_line **cache;
+
+void print_verbose(char *s)
+{
+    if(verbose)
+    {
+        printf(" %s",s);
+    }
+}
+// cache simulator
+void operate_cache(unsigned long long add)
+{
+    unsigned long long set_index=(add>>b)&((1<<s)-1);
+    unsigned long long line_tag=(add>>(b+s));
+    for(int j=0;j<E;j++)
+    {
+        if(cache[set_index][j].valid==1&&cache[set_index][j].tag==line_tag)
+        {
+            
+            print_verbose("hit");
+            hit_count++;
+            cache[set_index][j].lru_counter=time_counter;
+            return;
+        }
+    }
+    //not in cache
+    print_verbose("miss");
+    miss_count++;
+    for(int j=0;j<E;j++)
+    {
+        if(cache[set_index][j].valid==0)
+        {
+            cache[set_index][j].valid=1;
+            cache[set_index][j].tag=line_tag;
+            cache[set_index][j].lru_counter=time_counter;
+            return;
+        }
+    }
+    // cache is full
+    print_verbose("eviction");
+    eviction_count++;
+    int min_lru=cache[set_index][0].lru_counter,min_line=0;
+    for(int j=1;j<E;j++)
+    {
+        if(cache[set_index][j].lru_counter<min_lru)
+        {
+            min_lru=cache[set_index][j].lru_counter;
+            min_line=j;
+        }
+    }
+    cache[set_index][min_line].tag=line_tag;
+    cache[set_index][min_line].lru_counter=time_counter;
+}
 
 int main(int argc, char **argv)
 {
-    //handle command line args
-    int opt,verbose;
+    // handle command line args
+    hit_count=miss_count=eviction_count=0;
+    int opt;
     while (-1 != (opt = getopt(argc, argv, "h::v::s:E:b:t:")))
     {
         switch (opt)
@@ -44,14 +99,21 @@ int main(int argc, char **argv)
             break;
 
         case 's':
-            S = atoi(optarg); // string to int
+        {
+            s=atoi(optarg);
+            S = 1 <<s; // string to int,S=2^s
             break;
+        }
+
         case 'E':
             E = atoi(optarg);
             break;
         case 'b':
-            B = atoi(optarg);
+        {
+            b=atoi(optarg);
+            B = 1 << b; // 2^b bytes a block
             break;
+        }
         case 't':
         {
             if (strlen(optarg) >= 1024)
@@ -73,6 +135,69 @@ int main(int argc, char **argv)
         }
     }
 
+    // allocate and init cache
+    cache = (cache_line **)malloc(sizeof(cache_line *) * S);
+    for (int i = 0; i < S; i++)
+    {
+        cache[i] = (cache_line *)malloc(sizeof(cache_line) * E);
+        for (int j = 0; j < E; j++)
+        {
+            cache[i][j].valid=cache[i][j].lru_counter = cache[i][j].tag= 0;
+        }
+    }
+
+    FILE *pFile = fopen(filename, "r"); // open file for read
+    if(pFile==NULL)
+    {
+        printf("open trace file wrong!\n");
+        exit(-1);
+    }
+    char identifier;
+    unsigned long long  address;
+    int size; // Reading lines like " M 20,1" or "L 19,3"
+
+    while(fgets(buffer,1024,pFile))
+    {
+        if(buffer[0]=='I')
+        {
+            continue;
+        }
+        else
+        {
+            sscanf(buffer," %c %llx,%d",&identifier, &address, &size);
+            if(verbose)
+            {
+                printf("%s",buffer);
+            }
+            switch (identifier)
+            {
+            case 'L':
+                operate_cache(address);
+                break;
+            case 'M':
+                operate_cache(address);//operate cache twice
+            default:
+                operate_cache(address);
+                break;
+            }
+            time_counter++;
+            if(verbose)
+            {
+                printf("\n");
+            }
+        }
+        
+
+    }
+
+
+    fclose(pFile); // remember to close file when done
+
+    for(int i=0;i<S;i++)
+    {
+        free(cache[i]);
+    }
+    free(cache);
     printSummary(hit_count, miss_count, eviction_count);
     return 0;
 }
